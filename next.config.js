c06c75d26baf40526ccde777c4a11fd8fa559494
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-14 06:28:15 +0700

    chore: Remove unused code

--- next.config.js
+++ next.config.js
@@ -6,16 +6,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
-
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+module.exports = withSentryConfig(nextConfig)
