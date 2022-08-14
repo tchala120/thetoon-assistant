1e1b0de594c1cdfb8ba71f3916b2f004c89a9174
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-14 20:59:03 +0700

    feat: Split config for easy to read

--- environtment.d.ts
+++ environtment.d.ts
@@ -1,11 +1,28 @@
 namespace NodeJS {
   interface ProcessEnv {
-    NEXT_PUBLIC_SENTRY_DNS: string // this is the line you want
     NODE_ENV: 'development' | 'production'
     PORT?: string
     PWD: string
+
+    /**
+     * Sentry configuration env
+     */
+
     SENTRY_AUTH_TOKEN: string
+    NEXT_PUBLIC_SENTRY_DNS: string // this is the line you want
+
+    /**
+     * Telagram configuration env
+     */
+
     TELEGRAM_BOT_TOKEN: string
     TELEGRAM_CHANNEL_ID: string
+
+    /**
+     * Supabase configuration env
+     */
+    SUPABASE_URL: string
+    SUPABASE_ANON_PUBLIC_KEY: string
+    SUPABASE_SEVICE_ROLE_KEY: string
   }
 }
