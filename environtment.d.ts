4e4572812f046434e150750fe5882963903677f3
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-15 06:58:17 +0700

    feat: Added canonical env

--- environtment.d.ts
+++ environtment.d.ts
@@ -23,6 +23,7 @@ namespace NodeJS {
      */
     NEXT_PUBLIC_SUPABASE_URL: string
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+    NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY: string
     SUPABASE_SERVICE_ROLE_KEY: string
 
     /**
@@ -31,5 +32,10 @@ namespace NodeJS {
     LINE_API_KEY: string
     LINE_USER_ID: string
     LINE_CHANNEL_ACCESS_TOKEN: string
+
+    /**
+     * SEO configuration env
+     */
+    NEXT_PUBLIC_CANONICAL: string
   }
 }
