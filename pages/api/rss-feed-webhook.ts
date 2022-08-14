5cfcc8443604f71ed482afbefeafb76d9ed0c1bb
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-14 15:18:42 +0700

    feat: Added api to send message to telegram when new feed has updated

--- pages/api/rss-feed-webhook.ts
+++ pages/api/rss-feed-webhook.ts
@@ -1,11 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { withSentry } from '@sentry/nextjs'
+import { StatusCodes } from 'http-status-codes'
+
+import { sendMessage } from 'helpers/telegram'
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   console.log(request.body)
 
-  response.status(200).json({ name: 'John Doe' })
+  if (request.method === 'POST') {
+    sendMessage(request.body)
+      .then((data) => {
+        response.status(StatusCodes.OK).json({ message: 'Message sent!' })
+      })
+      .catch((error) => {
+        response
+          .status(StatusCodes.BAD_REQUEST)
+          .json({ message: 'Failed to sent message!', error })
+      })
+  } else {
+    response
+      .status(StatusCodes.METHOD_NOT_ALLOWED)
+      .json({ message: 'Method not allowed' })
+  }
 }
 
 export default withSentry(handler)
