4591f6f6d6a2bc7e5a624e1bc6cc09a73f2dce74
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-15 05:59:51 +0700

    chore: Removed log

--- pages/api/rss-feed-webhook.ts
+++ pages/api/rss-feed-webhook.ts
@@ -9,21 +9,15 @@ import { sendMessage } from 'helpers/telegram'
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   if (request.method === 'POST') {
-    console.log('News payload', request.body)
-
     await news.create(request.body)
 
     sendMessage(request.body)
       .then(({ data }) => {
         const body = { message: 'Message sent!', data }
 
-        console.log(body)
-
         response.status(StatusCodes.OK).json(body)
       })
       .catch((error) => {
-        console.log(error)
-
         response
           .status(StatusCodes.BAD_REQUEST)
           .json({ message: 'Failed to sent message!', error })
