e49fdd691eb676fbbd545225f9f8339e021be5b4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-14 15:16:45 +0700

    feat: Added crypto news type

---
+++ helpers/types.ts
@@ -0,0 +1,10 @@
+export interface CryptoNews {
+  title: string
+  url: string
+  author: string
+  content: string
+  imageUrl: string
+  published: string
+  feedTitle: string
+  feedUrl: string
+}
