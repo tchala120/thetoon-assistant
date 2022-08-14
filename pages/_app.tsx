3c4d49382fa745670a50f91645848347be893ec6
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-15 06:26:47 +0700

    feat: Wrapped with mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
-import '../styles/globals.css'
-
 import type { AppProps } from 'next/app'
 
+import { MantineProvider } from '@mantine/core'
+
+import RouterTransition from 'components/RouterTransition'
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <RouterTransition />
+
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
 }
 
 export default MyApp
