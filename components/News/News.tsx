8405f5e13a5841ad764853db6231000c5cc00a5c
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-16 04:44:15 +0700

    feat: Added author if possible

--- components/News/News.tsx
+++ components/News/News.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled'
-import { Badge, Card, Space, Text } from '@mantine/core'
+import { Badge, Card, Group, Space, Text } from '@mantine/core'
 import Link from 'next/link'
 
 import type { News as NewsType } from 'model/news'
@@ -14,9 +14,20 @@ const News = ({ data }: NewsProps) => {
       <Link href={`/news/${data.id}`}>
         <a style={{ textDecoration: 'none' }}>
           <Card>
-            <Text size="xs" color="#999">
-              From: {data.feedTitle}
-            </Text>
+            <Group spacing="xs">
+              <Text size="xs" color="#999">
+                From: {data.feedTitle}
+              </Text>
+
+              {data.author && (
+                <>
+                  -
+                  <Text size="xs" weight={500}>
+                    by {data.author}
+                  </Text>
+                </>
+              )}
+            </Group>
 
             <Text weight={500}>{data.title}</Text>
 
