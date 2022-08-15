ae346bc786e37cfb63c9523cb65de2d3b482f5de
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-15 11:17:24 +0700

    fix: Changed to site url instead

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,5 +1,5 @@
 export const meta = {
   title: 'TheToon Bot - Personal assistant to make life goes easier.',
   description: 'Crypto news aggregator to help me find news from everywhere.',
-  canonical: process.env.NEXT_PUBLIC_CANONICAL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
 }
