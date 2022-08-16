147ffdad6b0692f01c13c9acb295947563cf6d6e
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-16 23:39:58 +0700

    chore: Removed unused news crud handler

--- model/news.ts
+++ model/news.ts
@@ -1,6 +1,3 @@
-import dayjs from 'dayjs'
-import supabase from 'helpers/supabase'
-
 export interface News {
   id: number
   title: string
@@ -13,27 +10,3 @@ export interface News {
   feedUrl: string
   created_at: Date
 }
-
-const news = {
-  get: async () => {
-    const data = await supabase.from<News>('news')
-
-    const news = data.data?.sort((a, b) => {
-      const first = dayjs(a.created_at)
-      const second = dayjs(b.created_at)
-
-      return first.isBefore(second) ? 1 : -1
-    })
-
-    return {
-      ...data,
-      data: news,
-    }
-  },
-  getById: (id: number) =>
-    supabase.from<News>('news').select('*').match({ id }),
-  create: (record: News) => supabase.from<News>('news').insert(record),
-  delete: (id: number) => supabase.from('news').delete().match({ id }),
-}
-
-export default news
