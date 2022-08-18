23cc1767b16671c9b27ce3e7e8c7e5088511fc95
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-19 06:19:31 +0700

    feat: Changed meta content

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,5 +1,6 @@
 export const meta = {
-  title: 'TheToon Bot - Personal assistant to make life goes easier.',
-  description: 'Crypto news aggregator to help me find news from everywhere.',
+  title: 'TheToon Assistant - Personal assistant to make life goes easier.',
+  description:
+    'Assistant for myself. Just my playground do not use as an inspired 😅',
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
 }
