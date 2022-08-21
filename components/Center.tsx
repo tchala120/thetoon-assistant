3d56f7c10db3b92556a79568fa028ccc18841d52
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-21 12:40:06 +0700

    fix: Prioritized css

--- components/Center.tsx
+++ components/Center.tsx
@@ -4,9 +4,9 @@ const Center = styled.div`
   width: 100%;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
   text-align: center;
 `
 
