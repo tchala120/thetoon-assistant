167ea64a86fd66a3eb93e2f75da683398a515217
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-22 02:41:38 +0700

    feat: Wrapped by config provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,17 @@ import type { AppProps } from 'next/app'
 
 import { DefaultSeo } from 'next-seo'
 import NextNProgress from 'nextjs-progressbar'
+import { ConfigProvider } from 'antd-mobile'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import enUS from 'antd-mobile/es/locales/en-US'
 
 import { LiffProvider } from 'context/useLiffContext'
 
 import useSetupLiff from 'hooks/useSetupLiff'
 
 import { meta } from 'constants/meta'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { loading, liff, error } = useSetupLiff()
@@ -22,36 +27,40 @@ function MyApp({ Component, pageProps }: AppProps) {
   pageProps.liffError = error
 
   return (
-    <LiffProvider lineLiff={liff}>
-      <NextNProgress color="#1677ff" />
+    <ConfigProvider locale={enUS}>
+      <QueryClientProvider client={queryClient}>
+        <LiffProvider lineLiff={liff}>
+          <NextNProgress color="#1677ff" />
 
-      <DefaultSeo
-        canonical={meta.siteUrl}
-        openGraph={{
-          type: 'website',
-          locale: 'en_TH',
-          url: meta.siteUrl,
-          title: meta.title,
-          description: meta.description,
-          site_name: 'TheToon Assistant',
-          images: [
-            {
-              url: `${meta.siteUrl}/og-image.webp`,
-              alt: meta.description,
-              width: 800,
-              height: 600,
-            },
-          ],
-        }}
-        twitter={{
-          handle: '@thetoonishere',
-          site: '@thetoonishere',
-          cardType: 'summary_large_image',
-        }}
-      />
+          <DefaultSeo
+            canonical={meta.siteUrl}
+            openGraph={{
+              type: 'website',
+              locale: 'en_TH',
+              url: meta.siteUrl,
+              title: meta.title,
+              description: meta.description,
+              site_name: 'TheToon Assistant',
+              images: [
+                {
+                  url: `${meta.siteUrl}/og-image.webp`,
+                  alt: meta.description,
+                  width: 800,
+                  height: 600,
+                },
+              ],
+            }}
+            twitter={{
+              handle: '@thetoonishere',
+              site: '@thetoonishere',
+              cardType: 'summary_large_image',
+            }}
+          />
 
-      <Component {...pageProps} />
-    </LiffProvider>
+          <Component {...pageProps} />
+        </LiffProvider>
+      </QueryClientProvider>
+    </ConfigProvider>
   )
 }
 
