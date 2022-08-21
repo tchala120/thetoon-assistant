17a12b55e9a1f50bd4560fff4ec8b434f1379cab
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-21 14:49:18 +0700

    feat: Added line user id

--- helpers/line.ts
+++ helpers/line.ts
@@ -2,6 +2,7 @@ import { Client } from '@line/bot-sdk'
 
 export const lineChannelAccessToken =
   process.env.NEXT_PUBLIC_LINE_CHANNEL_ACCESS_TOKEN
+export const lineUserID = process.env.LINE_USER_ID
 
 const client = new Client({
   channelAccessToken: lineChannelAccessToken,
