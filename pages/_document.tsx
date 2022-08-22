8b8ec9a170d526969262f7df293fa00c95cda8cd
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-22 07:45:37 +0700

    feat: Added meta for support pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,30 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+const APP_NAME = 'TheToon Assistant'
+const APP_DESCRIPTION =
+  'TheToon Assistant - Personal assistant to makes my life goes eaiser.'
+
 const Document = () => {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content={APP_NAME} />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+        <meta name="description" content={APP_DESCRIPTION} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#FFFFFF" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/icons/icon-192x192.png"
+        />
+
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
