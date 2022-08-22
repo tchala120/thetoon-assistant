d7bd0274774223b9ac49cb31c8dbdff35f31eb37
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-08-22 07:42:57 +0700

    feat: Added safearea if want

--- components/BottomActionBar.tsx
+++ components/BottomActionBar.tsx
@@ -1,9 +1,30 @@
-import styled from '@emotion/styled'
+import type { ReactNode } from 'react'
 
-const BottomActionBar = styled.div`
+import styled from '@emotion/styled'
+import { SafeArea } from 'antd-mobile'
+
+interface BottomActionBarProps {
+  needSafeArea?: boolean
+  children: ReactNode
+}
+
+const BottomActionBar = ({
+  needSafeArea = true,
+  children,
+}: BottomActionBarProps) => {
+  return (
+    <>
+      <BottomActionBarContainer>{children}</BottomActionBarContainer>
+
+      {needSafeArea && <SafeArea position="bottom" />}
+    </>
+  )
+}
+
+export default BottomActionBar
+
+const BottomActionBarContainer = styled.div`
   flex: 0;
   padding: 12px;
   border-top: 1px solid var(--adm-color-border);
 `
-
-export default BottomActionBar
