b1d324aa9c3409a8ed3bbe226269f13afdbe7c9f
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 03:08:16 +0700

    feat: Added setting page

---
+++ src/pages/SettingPage.tsx
@@ -0,0 +1,11 @@
+import AppTabLayout from 'layouts/AppTabLayout'
+
+const SettingPage = () => {
+  return (
+    <AppTabLayout>
+      <h1>Setting Page</h1>
+    </AppTabLayout>
+  )
+}
+
+export default SettingPage
