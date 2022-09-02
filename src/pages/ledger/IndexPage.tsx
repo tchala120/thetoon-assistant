338c970b0d79ad1266627bcc257757e53cad1094
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 02:48:00 +0700

    feat: Added ledger page

---
+++ src/pages/ledger/IndexPage.tsx
@@ -0,0 +1,11 @@
+import AppLayout from 'layouts/AppLayout'
+
+const LedgerPage = () => {
+  return (
+    <AppLayout>
+      <h1>Ledger Index Page</h1>
+    </AppLayout>
+  )
+}
+
+export default LedgerPage
