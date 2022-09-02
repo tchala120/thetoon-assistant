c13b2b282649f94ebfe26eed758b8c68b0a33d55
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 03:07:16 +0700

    feat: Added bottom tab bar

---
+++ src/components/BottomTabBar.tsx
@@ -0,0 +1,46 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import { TabBar } from 'antd-mobile'
+import {
+  AppstoreOutline,
+  FileOutline,
+  SetOutline,
+  UnorderedListOutline,
+} from 'antd-mobile-icons'
+
+const BottomTabBar = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const tabs = [
+    {
+      key: '/',
+      title: 'Home',
+      icon: <AppstoreOutline />,
+    },
+    {
+      key: '/todo',
+      title: 'Todo',
+      icon: <UnorderedListOutline />,
+    },
+    {
+      key: '/ledger',
+      title: 'Ledger',
+      icon: <FileOutline />,
+    },
+    {
+      key: '/setting',
+      title: 'Setting',
+      icon: <SetOutline />,
+    },
+  ]
+
+  return (
+    <TabBar activeKey={pathname} onChange={(value) => navigate(value)}>
+      {tabs.map((item) => (
+        <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+      ))}
+    </TabBar>
+  )
+}
+
+export default BottomTabBar
