7f6741689c3ab9ae6fe6392c8f39d95ae845cb51
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 02:47:55 +0700

    feat: Added todo page

---
+++ src/pages/todo/IndexPage.tsx
@@ -0,0 +1,11 @@
+import AppLayout from 'layouts/AppLayout'
+
+const TodoPage = () => {
+  return (
+    <AppLayout>
+      <h1>Todo Index Page</h1>
+    </AppLayout>
+  )
+}
+
+export default TodoPage
