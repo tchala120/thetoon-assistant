04504ca981a79e48b1b68829930905da958c2f28
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 15:48:01 +0700

    feat: Added create todo popup to todo page

--- src/pages/todo/IndexPage.tsx
+++ src/pages/todo/IndexPage.tsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
+import { AddOutline } from 'antd-mobile-icons'
+
 import AppTabLayout from 'layouts/AppTabLayout'
 
+import CreateTodoPopup from 'components/CreateTodoPopup'
+
 const TodoPage = () => {
+  const [visible, setVisible] = useState(false)
+
   return (
-    <AppTabLayout>
+    <AppTabLayout
+      appBarActionMenus={
+        <AddOutline
+          fontSize={18}
+          color="var(--adm-color-primary)"
+          onClick={() => setVisible(true)}
+        />
+      }
+    >
       <h1>Todo Index Page</h1>
+
+      <CreateTodoPopup
+        visible={visible}
+        onClose={() => setVisible(false)}
+        onFinish={(values) => {
+          console.log(values)
+
+          setVisible(false)
+        }}
+      />
     </AppTabLayout>
   )
 }
