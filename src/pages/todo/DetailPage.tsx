89f8efb1a526597b659576e8f4d21925ba4ea56d
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 13:47:41 +0700

    feat: Added todo detail page

---
+++ src/pages/todo/DetailPage.tsx
@@ -0,0 +1,11 @@
+import AppLayout from 'layouts/AppLayout'
+
+const TodoDetailPage = () => {
+  return (
+    <AppLayout>
+      <h1>Todo Detail Page</h1>
+    </AppLayout>
+  )
+}
+
+export default TodoDetailPage
