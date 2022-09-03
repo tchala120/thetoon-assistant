962668d73b1c61c7d9dd0c44a13198945436dd89
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-03 13:46:22 +0700

    feat: Added reuse layout

---
+++ src/layouts/utils.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled'
+
+export const AppLayoutContainer = styled.div`
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+export const AppBarContainer = styled.div`
+  position: sticky;
+  top: 0;
+  flex: 0;
+  background: #fff;
+  border-bottom: solid 1px var(--adm-color-border);
+`
+
+export const AppBodyContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow-y: scroll;
+`
