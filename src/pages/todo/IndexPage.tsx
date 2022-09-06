e005c511a6382ee8bc776f685f45e1a90e2aa926
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-07 03:40:06 +0700

    feat: Added create todo mutation

--- src/pages/todo/IndexPage.tsx
+++ src/pages/todo/IndexPage.tsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
+import { PullToRefresh, Toast } from 'antd-mobile'
+
 import { AddOutline } from 'antd-mobile-icons'
 
 import AppTabLayout from 'layouts/AppTabLayout'
 
+import useTodos from 'react-query/useTodos'
+import useCreateTodo from 'react-query/useCreateTodo'
+
 import CreateTodoPopup, { useCreateTodoForm } from 'components/CreateTodoPopup'
+import TodoList from 'components/Todo'
 
 const TodoPage = () => {
+  const query = useTodos()
+  const mutation = useCreateTodo({
+    onSuccess() {
+      setVisible(false)
+
+      setTimeout(() => {
+        Toast.show({
+          icon: 'success',
+          content: (
+            <span
+              style={{
+                display: 'block',
+                textAlign: 'center',
+                wordBreak: 'break-word',
+              }}
+            >
+              Task create success!
+            </span>
+          ),
+        })
+      }, 250)
+    },
+  })
+
   const [form] = useCreateTodoForm()
 
   const [visible, setVisible] = useState(false)
@@ -20,17 +50,20 @@ const TodoPage = () => {
         />
       }
     >
-      <h1>Todo Index Page</h1>
+      <PullToRefresh onRefresh={() => query.refetch()}>
+        <TodoList
+          loading={query.isLoading}
+          data={query.data}
+          onOpen={() => setVisible(true)}
+        />
+      </PullToRefresh>
 
       <CreateTodoPopup
+        loading={mutation.isLoading}
         form={form}
         visible={visible}
         onClose={() => setVisible(false)}
-        onFinish={(values) => {
-          console.log(values)
-
-          setVisible(false)
-        }}
+        onFinish={mutation.mutate}
       />
     </AppTabLayout>
   )
