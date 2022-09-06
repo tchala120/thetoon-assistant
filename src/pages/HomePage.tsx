e8ebfe1a03fcf0647814d271375ff0be4edd4a7c
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-07 04:01:35 +0700

    feat: Added query from home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,12 @@
 import AppTabLayout from 'layouts/AppTabLayout'
 
+import useTodos from 'react-query/useTodos'
+
 const HomePage = () => {
+  const query = useTodos()
+
+  console.log('Query from home page', query.data)
+
   return (
     <AppTabLayout>
       <h1>Home Page</h1>
