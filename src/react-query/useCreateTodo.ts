e1d3f2b755f61717406b3b45d70542dc604bc87c
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-07 03:40:14 +0700

    feat: Added custom hook for create new todo

---
+++ src/react-query/useCreateTodo.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { useMutation } from '@tanstack/react-query'
+
+import type { MutationResponse, Todo } from './types'
+
+export interface CreateTodoVariable {
+  name: string
+  description: string
+  reminder: string
+}
+
+const useCreateTodo: MutationResponse<Todo, any, CreateTodoVariable> = (
+  options
+) =>
+  useMutation(
+    async (body) => {
+      const { data } = await axios.post('/todo', body)
+
+      return data
+    },
+    {
+      onError: (error) => {
+        console.log(error)
+      },
+      ...options,
+    }
+  )
+
+export default useCreateTodo
