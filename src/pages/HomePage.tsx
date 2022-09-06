11ad5f775ec651837ca91afcaa4a3ec59aeda18c
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-07 05:28:53 +0700

    chore: Removed unused code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,6 @@
 import AppTabLayout from 'layouts/AppTabLayout'
 
-import useTodos from 'react-query/useTodos'
-
 const HomePage = () => {
-  const query = useTodos()
-
-  console.log('Query from home page', query.data)
-
   return (
     <AppTabLayout>
       <h1>Home Page</h1>
