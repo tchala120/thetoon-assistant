cc9e2738c664fb1235cc1e112c1b31c3e9c23713
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-09 02:49:44 +0700

    feat: Added home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import { Space } from 'antd-mobile'
+
+import IncomingReminderTodos from './IncomingReminderTodos'
+
+const Home = () => {
+  return (
+    <Space block direction="vertical">
+      <IncomingReminderTodos />
+    </Space>
+  )
+}
+
+export default Home
