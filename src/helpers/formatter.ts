fa7db94540db726e3a9cee0f40c5d320c103c0b3
Panupong Tipjoi <itstoon.panupong@gmail.com>
2022-09-26 23:44:48 +0700

    feat: Added formatter

---
+++ src/helpers/formatter.ts
@@ -0,0 +1,5 @@
+export const DATE_FORMAT = 'DD/MM/YYYY'
+export const TIME_FORMAT = 'HH:mm'
+export const DATE_TIME_FORMAT = `${DATE_FORMAT} - ${TIME_FORMAT}`
+
+export const READABLE_DATE_FORMAT = 'ddd DD MMMM YYYY'
