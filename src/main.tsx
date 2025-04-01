6d559d748e5e6e43d64cdfab8e90a0f085c379bb
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-04-02 05:57:29 +0700

    feat(router): Add router provider into main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,25 @@
 import './global.css'
 
+import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import App from './App.tsx'
+import { routeTree } from './routeTree.gen.ts'
 
-createRoot(document.getElementById('root')!).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
-)
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
+
+const root = document.getElementById('root')
+
+if (root?.innerHTML != null) {
+	createRoot(root).render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>,
+	)
+}
