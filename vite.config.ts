60be1955aa906ed4135b8bb736dbeb2d6711fbc8
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-04-02 05:41:55 +0700

    feat(setup): Add tailwind css into vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -8,6 +9,7 @@ export default defineConfig({
 	plugins: [
 		react(),
 		tsconfigPaths(),
+		tailwindcss(),
 		VitePWA({
 			registerType: 'autoUpdate',
 			injectRegister: false,
