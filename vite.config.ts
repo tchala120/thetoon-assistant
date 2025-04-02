06667ae282e381dc4784e93e6b2a013146a22f24
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-04-02 07:00:07 +0700

    feat(setup): Update path ailas in tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +13,6 @@ export default defineConfig({
 			autoCodeSplitting: true,
 		}),
 		react(),
-		tsconfigPaths(),
 		tailwindcss(),
 		VitePWA({
 			registerType: 'autoUpdate',
@@ -45,4 +44,9 @@ export default defineConfig({
 			},
 		}),
 	],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
 })
