fe3a790a8d3661318bf8325bfd6e9e8e222095d4
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-04-02 07:36:12 +0700

    feat(setup): Wrap providers and add toaster and watch update pwa custom hook

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,22 @@
+import { useWatchUpdatePWA } from '@/hooks/useWatchUpdatePWA'
+import { Providers } from '@/providers'
 import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Toaster } from 'sonner'
 
 export const Route = createRootRoute({
 	component: Root,
 })
 
 function Root() {
+	useWatchUpdatePWA()
+
 	return (
-		<>
+		<Providers>
 			<div>Hello "__root"!</div>
 
 			<Outlet />
-		</>
+
+			<Toaster />
+		</Providers>
 	)
 }
