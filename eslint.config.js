7dce90d28dfa1cd83372b57ee3b73feaff26ef2e
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-04-02 07:34:36 +0700

    feat(react-query): Add eslint config for react query

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,12 @@
 import js from '@eslint/js'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
+	pluginQuery.configs['flat/recommended'],
 	{ ignores: ['dist', 'node_modules'] },
 	{
 		extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -17,6 +19,6 @@ export default tseslint.config(
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh,
 		},
-		rules: reactHooks.configs.recommended.rules
+		rules: reactHooks.configs.recommended.rules,
 	},
 )
