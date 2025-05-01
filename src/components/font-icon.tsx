bf13264a4e93d287d579b40f5c1a3e76abfc2086
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-05-02 06:15:27 +0700

    feat(font-icon): Export icon name type

--- src/components/font-icon.tsx
+++ src/components/font-icon.tsx
@@ -1,15 +1,17 @@
 import * as Icons from 'lucide-react'
 import { icons } from 'lucide-react'
 
+export type IconName = keyof typeof icons
+
 interface FontIconProps {
-	icon: keyof typeof icons
+	icon: IconName
 	size?: IconSize
 }
 
 export const FontIcon = ({ icon, size = 'medium' }: FontIconProps) => {
 	const Icon = Icons[icon]
 
-	return <Icon size={iconSizeToken[size]} />
+	return <Icon size={iconSizeToken[size]} className="text-inherit" />
 }
 
 type IconSize = 'small' | 'medium' | 'large'
