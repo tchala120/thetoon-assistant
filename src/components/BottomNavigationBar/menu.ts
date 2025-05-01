705db04b862061389d7e36844e50d93a69a27dc1
Panupong Tipjoi <itstoon.panupong@gmail.com>
2025-05-02 06:15:52 +0700

    feat(menu): Add navigation menu list

---
+++ src/components/BottomNavigationBar/menu.ts
@@ -0,0 +1,16 @@
+import { NavigationButtonItem } from './NavigationButton'
+
+export const navigationMenus: NavigationButtonItem[] = [
+	{
+		key: 'root',
+		label: 'Home',
+		pathname: '/',
+		iconName: 'House',
+	},
+	{
+		key: 'setting',
+		label: 'Setting',
+		pathname: '/setting',
+		iconName: 'Settings',
+	},
+]
